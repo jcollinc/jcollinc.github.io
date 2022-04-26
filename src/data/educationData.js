d028d369f2a742b8beca0dc08c7af67d5d4d2191
Jon-Collin Chinje <j.chinje@gmail.com>
2022-04-26 13:28:21 -0400

    switched education order, changed hosting location of demos

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,16 +1,16 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'George Mason University',
-        course: 'B.S. Economics',
-        startYear: '2012',
-        endYear: '2017'
-    },
-    {
-        id: 2,
         institution: 'Flatiron School',
         course: 'Software Engineering',
         startYear: '2022',
         endYear: '2022'
+    },
+    {
+        id: 2,
+        institution: 'George Mason University',
+        course: 'B.S. Economics',
+        startYear: '2012',
+        endYear: '2017'
     },
 ]
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -15,7 +15,7 @@ export const projectsData = [
         projectDesc: 'This project seeks to provide musicians with an easy-to-use layout for organizing practice routines, with the ability to easily track progress, and create lesson plans all in one place. It is my first full stack application developed solo from start to finish, and third full stack app overall.',
         tags: ['React', 'HTML' ,'CSS', 'Javascript', 'Ruby on Rails'],
         code: 'https://github.com/jcollinc/capstone',
-        demo: 'https://drive.google.com/file/d/1dHfwBemxM1-i9kAVt7iT3IBUmrq7I7vS/view',
+        demo: 'https://www.youtube.com/watch?v=lY9g-pIvGqk',
         image: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.15752-9/278908047_548158513359458_1212535711448942054_n.png?_nc_cat=104&ccb=1-5&_nc_sid=ae9488&_nc_ohc=Z41YPOEA0oQAX954Fb4&_nc_ht=scontent-iad3-1.xx&oh=03_AVL-QSfoULFc7L8I_6rOfHD47m7qizfU7DE9OqL9LMyVuw&oe=628B3C0D"
     },
     {
@@ -33,7 +33,7 @@ export const projectsData = [
         projectDesc: 'This project is a Spotify clone, and is my first full stack aplication. It was built with React and Sinatra. My partner and I learned a lot about how the front and back ends coordinate, as well as the considerations to be made when approaching the development of each.',
         tags: ['React', 'HTML' ,'CSS', 'Javascript', 'Ruby', 'Sinatra'],
         code: 'https://github.com/jcollinc/Dotify',
-        demo: 'https://drive.google.com/file/d/1n0oQHW0tn96VGyffUHDvRd3yWWDJdKYv/view',
+        demo: 'https://www.youtube.com/watch?v=PeQ3-VH7qQ4',
         image: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.15752-9/278366178_530182678510779_7321309932584804915_n.png?_nc_cat=104&ccb=1-5&_nc_sid=ae9488&_nc_ohc=2v4mmSZdvQ0AX8-wPGM&_nc_ht=scontent-iad3-1.xx&oh=03_AVJKExzL4bfTalHqqu-9sy0iPL3OU54uGrqjlVixK-0QnA&oe=628B7684"
     },
     {
@@ -42,7 +42,7 @@ export const projectsData = [
         projectDesc: 'A message board created for classmates to collaborate on work, with a coffee ordering component; built using React. My partner and I learned about RESTful conventions and used json-server to create a REST API for out React front end.',
         tags: ['React', 'HTML' ,'CSS', 'Javascript'],
         code: 'https://github.com/jcollinc/Study-Room',
-        demo: 'https://drive.google.com/file/d/1yLN6w_yeOXn62S0Qk6S1XhLSBIBBN_C6/view',
+        demo: 'https://www.youtube.com/watch?v=Xx0XEUWkUIY',
         image: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.15752-9/278139628_503982587873192_5127926746016566799_n.png?_nc_cat=104&ccb=1-5&_nc_sid=ae9488&_nc_ohc=DM7wAu8yVG0AX_S6eSD&_nc_ht=scontent-iad3-1.xx&oh=03_AVJ1eU1bVL3vzN_duuJrY5Avp9K-Zgib6qnWZJptWaibdw&oe=628B5B6C"
     },
     
