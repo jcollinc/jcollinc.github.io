d233d7049b21124f07d5b0a486a3f1553ea56bed
Jon-Collin Chinje <j.chinje@gmail.com>
2022-04-25 21:17:57 -0400

    minor profile edits

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Salut!",
-    description1: "My name is Jon-Collin Chinje - I'm an aspiring web developer based in New York.",
-    description2: "I am a recent graduate of Flatiron School, where I learned full stack web development using Javascript and Ruby on Rails. I fully believe in the concept of life-long learning, and development offers an infinitely growing pool of new and interesting concepts - I am so excited to explore it to the fullest. ",
+    description1: "My name is Jon-Collin Chinje - I'm an aspiring software engineer based in New York.",
+    description2: "I am a recent graduate of Flatiron School, where I learned full stack development using Javascript and Ruby on Rails. I fully believe in the concept of life-long learning, and development offers an infinitely growing pool of new and interesting concepts - I am so excited to explore it to the fullest. ",
     image: 2
 }
